5309f995585ec7e8160ced09983ba93a1982d020
takbirgazi <takbirgazibd@gmail.com>
2025-09-14 18:07:39 +0600

    Add post API

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import compression from "compression";
 import cors from "cors";
 import express from "express";
 import { userRouter } from "./modules/user/user.route";
+import { postRouter } from "./modules/post/post.route";
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.get("/", (_req, res) => {
 });
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/post", postRouter);
 
 
 // 404 Handler
